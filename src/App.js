70a421b424a0a2801380d667013f8b4c50f414e1
v1itoof <joaov1c@outlook.com>
2024-04-22 10:00:53 -0300

    Routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 
+import history from './services/history';
 import Login from './pages/Login';
 import Header from './components/Header';
+import Routes from './routes';
 
 import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
-    <>
+    <Router history={history}>
       <Header />
-      <Login />
+      <Routes />
       <GlobalStyles />
-    </>
+    </Router>
   );
 }
 
