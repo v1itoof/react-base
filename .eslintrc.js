948c69e8cd82148b36fb948e8134acb6aab37a2c
v1itoof <joaov1c@outlook.com>
2024-04-22 09:08:11 -0300

    Cores da aplicação

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'react-app',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+    ecmaVersion: 'latest',
+    requireConfigFile: false,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'no-unused-vars': 'off',
+  },
+};
---
+++ src/config/colors.js
@@ -0,0 +1,7 @@
+export const primaryColor = `#C3073F`;
+export const primaryDarkColor = `#1A1A1D`;
+
+export const successColor = `#0197F6`;
+export const infoColor = `#116ABC`;
+export const errorColor = `#F2AF29`;
+export const warningColor = `#F4CB05`;
